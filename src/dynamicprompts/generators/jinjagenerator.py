1905d6e013e5cdd05d76321213f65a0655eeb892
Aarni Koskela <akx@iki.fi>
2023-01-26 22:59:59 +0200

    Make mypy happy about Jinja generator

--- src/dynamicprompts/generators/jinjagenerator.py
+++ src/dynamicprompts/generators/jinjagenerator.py
@@ -4,16 +4,19 @@ import logging
 import random
 import re
 from itertools import permutations
+from typing import Any, Callable, Iterable, cast
 
-import jinja2.nodes
 from jinja2 import Environment
 from jinja2.exceptions import TemplateSyntaxError
 from jinja2.ext import Extension
+from jinja2.nodes import CallBlock
+from jinja2.parser import Parser
 
 from dynamicprompts.generators.promptgenerator import (
     GeneratorException,
     PromptGenerator,
 )
+from dynamicprompts.wildcardmanager import WildcardManager
 
 logger = logging.getLogger(__name__)
 
@@ -22,17 +25,17 @@ re_combinations = re.compile(r"\{([^{}]*)}")
 
 
 class RandomExtension(Extension):
-    def __init__(self, environment):
+    def __init__(self, environment: Environment) -> None:
         super().__init__(environment)
         environment.globals["choice"] = self.choice
         environment.globals["weighted_choice"] = self.weighted_choice
         environment.globals["random"] = self.random
         environment.globals["randint"] = self.randint
 
-    def choice(self, *items):
+    def choice(self, *items) -> Any:
         return random.choice(items)
 
-    def weighted_choice(self, *items):
+    def weighted_choice(self, *items) -> Any:
         population, weights = zip(*items)
         return random.choices(population, weights=weights)[0]
 
@@ -44,12 +47,17 @@ class RandomExtension(Extension):
 
 
 class PermutationExtension(Extension):
-    def __init__(self, environment):
+    def __init__(self, environment: Environment) -> None:
         super().__init__(environment)
         environment.globals["permutations"] = self.permutation
 
-    def permutation(self, items, low: int, high: int | None = None):
-        vars = []
+    def permutation(
+        self,
+        items: Iterable[Any],
+        low: int,
+        high: int | None = None,
+    ) -> list[Any]:
+        vars: list[Any] = []
         if high is None:
             high = low
 
@@ -60,12 +68,12 @@ class PermutationExtension(Extension):
 
 
 class WildcardExtension(Extension):
-    def __init__(self, environment):
+    def __init__(self, environment: Environment) -> None:
         super().__init__(environment)
         environment.globals["wildcard"] = self.wildcard
 
-    def wildcard(self, wildcard_name):
-        wm = self.environment.wildcard_manager
+    def wildcard(self, wildcard_name: str) -> list[str]:
+        wm = cast(WildcardManager, self.environment.globals["wildcard_manager"])
         values = []
 
         for value in wm.get_all_values(wildcard_name):
@@ -84,27 +92,38 @@ class WildcardExtension(Extension):
 class PromptExtension(Extension):
     tags = {"prompt"}
 
-    def __init__(self, environment):
+    def __init__(self, environment: Environment) -> None:
         super().__init__(environment)
         environment.extend(prompt_blocks=[])
 
-    def parse(self, parser):
+    def parse(self, parser: Parser) -> CallBlock:
         lineno = next(parser.stream).lineno
-        body = parser.parse_statements(["name:endprompt"], drop_needle=True)
-        return jinja2.nodes.CallBlock(
+        body = parser.parse_statements(
+            ("name:endprompt",),
+            drop_needle=True,
+        )
+        node = CallBlock(
             self.call_method("_prompt", []),
             [],
             [],
             body,
-        ).set_lineno(lineno)
+        )
+        node.set_lineno(lineno)
+        return node
 
-    def _prompt(self, caller):
-        self.environment.prompt_blocks.append(caller())
-        return caller()
+    def _prompt(self, caller: Callable) -> Any:
+        value = caller()
+        prompt_blocks = cast(list, self.environment.globals["prompt_blocks"])
+        prompt_blocks.append(value)
+        return value
 
 
 class JinjaGenerator(PromptGenerator):
-    def __init__(self, wildcard_manager=None, context=None):
+    def __init__(
+        self,
+        wildcard_manager: WildcardManager | None = None,
+        context: dict | None = None,
+    ) -> None:
         self._wildcard_manager = wildcard_manager
 
         if context is not None:
@@ -113,26 +132,33 @@ class JinjaGenerator(PromptGenerator):
             self._context = {}
 
     def generate(self, template: str, num_prompts: int = 1) -> list[str]:
+        env = Environment(
+            extensions=[
+                RandomExtension,
+                PromptExtension,
+                WildcardExtension,
+                PermutationExtension,
+            ],
+        )
+        prompt_blocks: list[str] = []
+        env.globals.update(
+            {
+                "wildcard_manager": self._wildcard_manager,
+                "prompt_blocks": prompt_blocks,
+            },
+        )
+
         try:
-            env = Environment(
-                extensions=[
-                    RandomExtension,
-                    PromptExtension,
-                    WildcardExtension,
-                    PermutationExtension,
-                ],
-            )
-            env.wildcard_manager = self._wildcard_manager
-
-            prompts = []
-            for i in range(num_prompts):
-                jinja_template = env.from_string(template)
-                s = jinja_template.render(**self._context)
-                prompts.append(s)
-
-            if env.prompt_blocks:
-                prompts = env.prompt_blocks
-            return prompts
+            jinja_template = env.from_string(template)
         except TemplateSyntaxError as e:
             logger.exception(e)
             raise GeneratorException(e.message) from e
+
+        prompts = []
+        for i in range(num_prompts):
+            s = jinja_template.render(**self._context)
+            prompts.append(s)
+
+        if prompt_blocks:
+            prompts = prompt_blocks
+        return prompts
