f4d374d9fe730c7ff1fc5a44324dc18b86d6b18e
Aarni Koskela <akx@iki.fi>
2023-01-30 10:08:49 +0200

    Actually test AttentionGenerator

--- tests/prompts/generators/test_attentiongenerator.py
+++ tests/prompts/generators/test_attentiongenerator.py
@@ -1,13 +1,11 @@
 import pytest
-from dynamicprompts.generators.attentiongenerator import (
-    AttentionGenerator,
-    DummyGenerator,
-)
+from dynamicprompts.generators.attentiongenerator import AttentionGenerator
 
 
 @pytest.mark.slow
-class TestAttentionGenerator:
-    def test_default_generator(self):
-        pytest.importorskip("spacy")
-        generator = AttentionGenerator()
-        assert isinstance(generator._prompt_generator, DummyGenerator)
+def test_default_generator():
+    pytest.importorskip("spacy")
+    generator = AttentionGenerator()
+    for prompt in generator.generate("purple cat singing opera, artistic, painting", 5):
+        assert "(purple" in prompt or "(artistic" in prompt or "(painting" in prompt
+        assert ")" in prompt
