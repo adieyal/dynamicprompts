61c413a95b3582ecc0d50ae256158fab237172b4
Aarni Koskela <akx@iki.fi>
2023-03-26 14:14:09 +0300

    Add utility to deal with A1111 special syntax chunks

--- src/dynamicprompts/constants.py
+++ src/dynamicprompts/constants.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import re
 from random import Random
 
 DEFAULT_ENCODING = "utf-8"
@@ -7,3 +8,6 @@ DEFAULT_COMBO_JOINER = ","
 MAX_IMAGES = 1000
 WILDCARD_SUFFIX = "txt"
 DEFAULT_RANDOM = Random()
+
+# A1111 special syntax (LoRA, hypernet, etc.)
+A1111_SPECIAL_SYNTAX_RE = re.compile(r"\s*<[^>]+>")
--- src/dynamicprompts/utils.py
+++ src/dynamicprompts/utils.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 import random
+import re
 from collections import OrderedDict
 from itertools import cycle
 from typing import Iterable, TypeVar
 
+from dynamicprompts.constants import A1111_SPECIAL_SYNTAX_RE
 from dynamicprompts.types import StringGen
 
 T = TypeVar("T")
@@ -91,3 +93,25 @@ def choose_without_replacement(
             weights.remove(weights[values.index(chosen)])
             values.remove(chosen)
         return chosen_values
+
+
+def remove_a1111_special_syntax_chunks(s: str) -> tuple[str, list[str]]:
+    """
+    Remove A1111 special syntax chunks from a string and return the string and the chunks.
+    """
+    chunks: list[str] = []
+
+    def put_chunk(m):
+        chunks.append(m.group(0))
+        return ""
+
+    return re.sub(A1111_SPECIAL_SYNTAX_RE, put_chunk, s), chunks
+
+
+def append_chunks(s: str, chunks: list[str]) -> str:
+    """
+    Append (A1111 special syntax) chunks to a string.
+    """
+    if not chunks:
+        return s
+    return f"{s}{''.join(chunks)}"
